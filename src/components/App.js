50e68334a7b1cb03b00c7f35b47b7dd6b2820e1f
Jerry Tong <zihangtong@gmail.com>
2021-12-20 13:51:42 -0500

    commit tree 1b5d0e9c58c018d44c72490e6ae56b7ab4ae4049

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            setData(data.message)
+            setIsLoaded(true);
+        })
+    }, [])
+console.log(data)
+    return (
+        isLoaded
+        ? <img alt="A Random Dog" src={data}></img>
+        : <p>Loading...</p>
+    )
+}
+
+export default App;
